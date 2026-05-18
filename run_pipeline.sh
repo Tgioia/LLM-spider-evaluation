20a64f640c664eaad6ac2e1db41e6c6be148b852
Tgioia <103372001+Tgioia@users.noreply.github.com>
2026-05-18 14:10:34 +0200

    Add additional Table-QA scripts to pipeline

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -6,6 +6,8 @@ python Text_to_SQL.py
 python TtS_exec_and_norm.py
 echo "Running Table-QA..."
 python Table_QA.py
+python Table_QA_simple.py
+python Table_QA_COT.py
 
 echo "Evaluating the metrics"
 python evaluate_metrics.py
